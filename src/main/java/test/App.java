5e8944143537777e3f079f449e40faf26654f75f
RosesTheN00b <sebastian.ruziczka@gmail.com>
2015-02-23 09:15:16 +0100

    Verknüpfung vom CLose-Request mit dem Thread-State

--- src/main/java/test/App.java
+++ src/main/java/test/App.java
@@ -7,12 +7,11 @@ import java.util.Vector;
 
 public class App {
 	private Vector<HandlerRunnable> listeMitThreads = new Vector<HandlerRunnable>();
-	private boolean closeRequested = false;
 
 	private App() throws IOException {
 		ServerSocket server = new ServerSocket(4321);
 
-		while (!closeRequested) {
+		while (!isCloseRequested()) {
 			Socket client = server.accept();
 			HandlerRunnable newHandler = new HandlerRunnable(client, this);
 			Thread thread = new Thread(newHandler);
@@ -20,6 +19,14 @@ public class App {
 			thread.start();
 		}
 		server.close();
+	}
+
+	private boolean isCloseRequested() {
+		Thread actualThread = Thread.currentThread();
+		if (actualThread.getState().equals(Thread.State.TERMINATED)) {
+			return true;
+		}
+		return false;
 	}
 
 	public static void main(String[] args) throws IOException {
