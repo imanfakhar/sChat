1a83893dd1385df5c75d7049a1dfbd98dc19256d
RosesTheN00b <sebastian.ruziczka@gmail.com>
2015-02-23 09:09:32 +0100

    Soched Closed

--- src/main/java/test/App.java
+++ src/main/java/test/App.java
@@ -7,17 +7,19 @@ import java.util.Vector;
 
 public class App {
 	private Vector<HandlerRunnable> listeMitThreads = new Vector<HandlerRunnable>();
+	private boolean closeRequested = false;
 
 	private App() throws IOException {
 		ServerSocket server = new ServerSocket(4321);
 
-		while (true) {
+		while (!closeRequested) {
 			Socket client = server.accept();
 			HandlerRunnable newHandler = new HandlerRunnable(client, this);
 			Thread thread = new Thread(newHandler);
 			listeMitThreads.add(newHandler);
 			thread.start();
 		}
+		server.close();
 	}
 
 	public static void main(String[] args) throws IOException {
